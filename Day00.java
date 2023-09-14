177b00f29ede10df3e4db4255252cf9d0f28529a
sherlynt <serr5mm@gmail.com>
2023-09-14 18:52:18 +0700

    first task done

---
+++ Day00.java
@@ -0,0 +1,12 @@
+public class Day00 {
+    public static void main(String[] args) {
+        int a = 479598;
+        int result = 0;
+        while (a != 0) {
+            int remainder  = a%10;
+            result +=remainder;
+            a /= 10;
+        }
+        System.out.println(result);
+    }
+}
