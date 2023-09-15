35cbe938e6a7555a177c880e941376e80d3f5b5b
sherlynt <serr5mm@gmail.com>
2023-09-15 23:02:40 +0700

    ex01 ex02 start

---
+++ Day00/ex02/Program.java
@@ -0,0 +1,15 @@
+package Day00.ex02;
+import java.util.*;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+//
+//        while (scanner.hasNext()) {
+//
+//        }
+
+        // close the scanner
+        scanner.close();
+    }
+}
