ca919bcd72a638a87e52ebd62dff3cbcf5d52c1e
sherlynt <serr5mm@gmail.com>
2023-09-16 20:33:23 +0700

    ex01 ex03 start

---
+++ Day00/ex03/Program.java
@@ -0,0 +1,25 @@
+package Day00.ex03;
+
+import java.util.Scanner;
+
+public class Program {
+
+//    static final int MAX_WEEKS = 18;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String string = scanner.nextLine();
+            if (string.equals("Week")) {
+                System.out.println("kukuk");
+            } else {
+                System.out.println("govno");
+                break;
+            }
+
+
+        }
+
+    }
+}
